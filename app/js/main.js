8809cb3bde1d0f0198d903d4f49e0e811cd5cbaa
Roman-Moses <mosesroman681@gmail.com>
2024-09-19 18:49:06 +0300

    second changes

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+
+//changing navbar color when scrolling
+
+
+window.addEventListener("scroll",() => {
+    document.querySelector("nav").classList.toggle
+    ("window-scroll", window.scrollY > 0)
+});
