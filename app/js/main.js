b7dbf41c3cefe200f6cb0d6614d2885408f0d58e
Roman-Moses <mosesroman681@gmail.com>
2024-09-23 18:11:57 +0300

    end homepage

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,50 @@
 
-//changing navbar color when scrolling
-
+//start changing navbar color when scrolling
 
 window.addEventListener("scroll",() => {
     document.querySelector("nav").classList.toggle
     ("window-scroll", window.scrollY > 0)
 });
+// end changing navbar color when scrolling
+
+
+
+//start showing more on cards section
+
+const fags = document.querySelectorAll(".fag")
+fags.forEach( fag =>{
+    fag.addEventListener("click", () =>{
+        fag.classList.toggle("open")
+
+        //start change icon in card area
+
+        const icon = fag.querySelector(".fag-icon i");
+        if(icon.className === "fa-solid fa-plus") {
+            icon.className = "fa-solid fa-minus"
+        }
+        else(
+            icon.className ="fa-solid fa-plus"
+        );
+        // start change icon in card area
+
+    });
+});
+// end showing more on cards section
+
+// start  swiper section on homepage
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // when the width is greater yhan 600px
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        }
+    }
+  });
+//   end  swiper section on homepage
